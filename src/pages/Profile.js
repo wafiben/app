a639b3f84eab574645955721ca31726b50945bdc
wafiben <wafibenjeddou@gmail.com>
2023-03-01 21:57:14 +0100

    sfsf

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,11 +3,7 @@ import { useEffect } from "react";
 import axios from "axios";
 
 function Profile() {
-  useEffect(() => {
-    axios.get("http://localhost:4000/user").then((data) => {
-      console.log(data);
-    });
-  }, []);
+ 
   return <div>Profile</div>;
 }
 
